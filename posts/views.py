590d7456fd0423ec4f958eb4d5c75cb3436bfe4e
Kira <kirasuperkira21@gmail.com>
2025-10-01 19:54:48 +0700

    feat: add Post model with tests

--- posts/views.py
+++ posts/views.py
@@ -2,3 +2,6 @@ from django.http import HttpResponse
 
 def home_view(request):
     return HttpResponse("<h1>Добро пожаловать в мой блог!</h1>")
+
+def about_view(request):
+    return HttpResponse("<h1>Вторая страница</h1>")
