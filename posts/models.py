6f0abf9c6132820036900f179f3055c3f8a36f8a
Kira <kirasuperkira21@gmail.com>
2025-10-02 17:03:25 +0700

    feat: add post list view with tests

--- posts/models.py
+++ posts/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.utils import timezone
 
 class Post(models.Model):
+    id = models.IntegerField(max_length=20, primary_key=True)
     title = models.CharField(max_length=200)
     content = models.TextField()
     created_at = models.DateTimeField(default=timezone.now)
--- posts/views.py
+++ posts/views.py
@@ -12,3 +12,11 @@ def post_list_view(request):
         html += f"<li><strong>{post.title}</strong>: {post.content[:50]}...</li>"
     html += "</ul>"
     return HttpResponse(html)
+
+def post_id_view(request):
+    posts = Post.objects.get(id=post_id)
+    html = "<h1>Пост по id</h1><ul>"
+    for post in posts:
+        html += f"<li><strong>{post.id}</strong>: {post.title}, {post.content[:50]}...</li>"
+    html += "</ul>"
+    return HttpResponse(html)
